03f48591c6b4d5f1b757ff8446d2e8847ca33f29
Daniel Hall <snake05865@mail.ru>
2023-11-30 21:04:17 +0300

    Added removeUserById method

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,18 +2,18 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl(new Util());
         userDao.createUsersTable();
         userDao.saveUser("Вася", "Иванов", (byte) 20);
         userDao.saveUser("Петя", "Петров", (byte) 24);
         userDao.saveUser("Коля", "Сидоров", (byte) 25);
         userDao.saveUser("Дима", "Пушкин", (byte) 19);
-
-        //todo: п. Удаление User из таблицы ( по id )
+        userDao.removeUserById(4);
         System.out.println(userDao.getAllUsers());
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
